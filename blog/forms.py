408263c38238806b822f097fd08ba55284a13e48
Jon Kiparsky <jon@kiparsky.net>
2017-10-17 22:44:25 -0400

    Add countries

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ from .models import Post
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text', "country")
--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,23 @@
 from django.utils import timezone
 from django.db.models import (
+    CASCADE,
     CharField,
     DateTimeField,
     ForeignKey,
     Model,
     TextField,
 )
+from django_countries.fields import CountryField
 
 class Post(Model):
-    author = ForeignKey('auth.User')
+    author = ForeignKey('auth.User',
+                        CASCADE)
     title = CharField(max_length=200)
     text = TextField()
     created_at = DateTimeField(default=timezone.now)
     published_date = DateTimeField(blank=True,
                                    null=True)
+    country = CountryField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
